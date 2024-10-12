c5f7abf0a60296f87972a55478765850403e0fdb
Your Name <you@example.com>
2024-10-12 10:31:01 -0400

    code changes

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
+//http://localhost:3000/
+app.get('/name', function (req, res) {
+  res.send("<h1>c0906722- Shivraj_layra</h1>")
+})
+
 //http://localhost:3000/profile
 app.post('/profile', (req, res) => {
   console.log(req.body)
